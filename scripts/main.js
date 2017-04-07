2802632fa38946f2242bbf0cf99e5f21b1b06720
zk zheng <1028849434@qq.com>
2017-04-07 16:44:02 +0800

    another commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,31 @@
 var myHeading = document.querySelector('h1');
 myHeading.innerHTML = 'Hello Zheng!';
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function(){
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === '/web-projects/test-site/images/timg.jpg'){
+      myImage.setAttribute ('src','/web-projects/test-site/images/timg2.jpg');
+    }else{
+      myImage.setAttribute ('src','/web-projects/test-site/images/timg.jpg');
+    }
+}
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
+
